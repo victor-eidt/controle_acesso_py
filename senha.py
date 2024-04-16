5104fb31afd2652b811e28fd668092292174827b
victor-eidt <victoreidtrl@gmail.com>
2024-04-16 20:22:19 -0300

    menu pós login

--- senha.py
+++ senha.py
@@ -1,3 +1,5 @@
+### Victor Gabriel Eidt e Giordano Diniz Serafini ###
+
 from getpass import getpass
 
 with open("usuarios.txt", "r") as arquivo:
@@ -23,7 +25,23 @@ def ler_tentativas():
 def registrar_tentativa(login):
     with open("tentativas.txt", "a") as arquivo:
         arquivo.write(f"{login}\n")
+        
+def menu_inicial():
+    
+    selecao = int(input("What's the move?\n(1) Cadastrar\n(2) Autenticar\n(3) Sair\nDigite uma opção: "))
+    
+    match selecao:
 
+        case 1:
+            "cadastro()"
+            
+        case 2:
+            aut(usuarios)
+            
+        case 3:
+            exit
+            
+            
 def aut(usuarios):
     tentativas = ler_tentativas()
 
@@ -39,12 +57,16 @@ def aut(usuarios):
 
         if (login, senha) in usuarios:
             print(f"Seja bem vindo, {login}")
+            menu_usuario()
             break
+        
         else:
             print("Usuário Inexistente ou senha incorreta")
             registrar_tentativa(login)
 
         if i == 4:
             print("Limite de tentativas excedida, conta bloqueada temporariamente")
-
-aut(usuarios)
+            
+def menu_usuario():
+    
+    selecao = input("What's the move?\n(1) listar arquivos\n(2) criar arquivo\n(3) ler arquivo\n(4) excluir arquivo\n(5) executar arquivo\n(6) Desbloquear Usuário\n(7) sair\nDigite uma opção: ")
